67cffff997b726044f92c72e7d075cd8a522fb15
ks0ve <49819257+ks0ve@users.noreply.github.com>
2023-01-26 11:38:11 -0500

    Add files via upload

---
+++ Wordle.java
@@ -0,0 +1,31 @@
+public class Game {
+
+    final String wordle = "guess";
+
+    public char[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+    private int count = 0;
+
+    public char[] convertGuess(userInput) {
+
+        
+
+        return arrayInput
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game Wordle = new Game;
+
+        while (Wordle.userGuess) {
+
+            Wordle.userGuess;
+
+        }
+
+    }
+
+}
